b136aaaf351ef778f110ea038ab6bc612b7026a7
zichou.zy <zichou.zy@alibaba-inc.com>
2020-03-05 20:43:47 +0800

    fix tab switch issue

--- www/src/App.js
+++ www/src/App.js
@@ -17,19 +17,19 @@ function App() {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['client']}
+            defaultSelectedKeys={[window.location.hash.substr(1) === "/" ? "/client" : window.location.hash.substr(1)]}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="client"><Link to='/client'>clients</Link></Menu.Item>
-            <Menu.Item key="server"><Link to='/server'>servers</Link></Menu.Item>
+            <Menu.Item key="/client"><Link to='/client'>clients</Link></Menu.Item>
+            <Menu.Item key="/server"><Link to='/server'>servers</Link></Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px', marginTop: 64 }}>
           <div style={{ background: '#fff', padding: 24, minHeight: 480 }}>
             <Switch>
-              <Route key='client' path='/' exact component={ClientApp}/>
-              <Route key='client' path='/client' exact component={ClientApp}/>
-              <Route key='server' path='/server' exact component={ServerApp}/>
+              <Route key='/client' path='/' exact component={ClientApp}/>
+              <Route key='/client' path='/client' exact component={ClientApp}/>
+              <Route key='/server' path='/server' exact component={ServerApp}/>
             </Switch>
           </div>
         </Content>
